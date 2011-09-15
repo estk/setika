f88d3a877df1d992e0cc9235b440ed49259c5865
evan <esims89@gmail.com>
2011-09-14 17:09:07 -0700

    Working

--- setika.rb
+++ setika.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
 require 'sinatra'
+require 'sass'
+
+before do
+  @title = "Setika.me"
+end
+
+helpers do
+  def render_flickr_slides
+    erb %Q( <div id="flickr_slides"><img src="http://farm1.static.flickr.com/55/148800272_86cffac801.jpg" /></div> )
+  end
+end
+
 
 get '/' do
   erb :index
+end
+
+get '/style.css' do
+  scss :style
 end
